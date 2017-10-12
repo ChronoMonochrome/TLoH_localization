d2a188bdbae2c4acd84d88e8df70732949c10e1e
Shilin Victor <chrono.monochrome@gmail.com>
2017-10-12 12:31:55 +0300

    Add support for *.tbl files containing various tags

--- translate.py
+++ translate.py
@@ -6,11 +6,12 @@ from collections import OrderedDict
 TBL_SIGNATURE_LEN = 2
 HEADER_TAG = "header"
 ENTRY_TAG = "Entry"
+GROUP_TAG = "Group"
 
-ptrns = ["[\xe0-\xef].?.?", ".?\x01", "%[ds]", "#[0-9]*C", "#[0-9a-f]*"]
-entry_ptrn = "(%s)" % ("|".join(ptrns))
+common_entry_ptrns = ["[\xe0-\xef].?.?", ".?\x01", "%[ds]", "#[0-9]*C", "#[0-9a-f]*"]
+common_entry_ptrn = "(%s)" % ("|".join(common_entry_ptrns))
 
-_match = lambda s: any([re.match(i, s) for i in ptrns])
+_match = lambda s, ptrns: any([re.match(i, s) for i in ptrns])
 
 def u_test(s, enc = "shift_jis"):
 	"""Try converting string to unicode. 
@@ -35,10 +36,10 @@ def _get_tbl_tag(in_file, is_tbl_file, tbl_tag_max_len = 200):
 		el_header = doc.find(HEADER_TAG)
 		return el_header.get("type")
 
-def read_tbl(in_file, t_tbl_tag = ""):
-	"""Read binary *.tbl file. 
+def read_tbl(in_file, l_tbl_tags):
+	"""Read binary *.tbl file.
 	Returns *.tbl header and list of OrderedDict's representing each text or binary data entry."""
-	
+
 	def _split(s):
 		text_start = s[:-1].rfind("\x00") + 1
 		data = s[:text_start]
@@ -47,41 +48,49 @@ def read_tbl(in_file, t_tbl_tag = ""):
 
 	res = []
 	data = open(in_file, "rb").read()
+	ptrn = "(%s)" % "|".join(l_tbl_tags)
+	raw_strings = re.split(ptrn, data)
 
-	if not t_tbl_tag:
-		t_tbl_tag = _get_tbl_tag(in_file, is_tbl_file = True)
-
-	raw_strings = data.split(t_tbl_tag)
-	
 	header = OrderedDict()
 	header["data"] = b64encode(raw_strings[0])
-	header["type"] = t_tbl_tag
-	
+
+	tag_type = ""
 	for raw_string in raw_strings[1:]:
 		entry_group = OrderedDict()
-		data, raw_text = _split(raw_string)
+		# If raw_string doesn't match with any tag, 
+		# just proceed it as an entry text. Otherwise,
+		# remember a tag type and proceed the next raw_string.
+		if not raw_string in l_tbl_tags:
+			data, raw_text = _split(raw_string)
+		else:
+			tag_type = raw_string
+			continue
+	
 		entry_group["data"] = b64encode(data)
 		entry_group["entries"] = []
-		
-		for entry_text in re.split(entry_ptrn, raw_text):
+		assert(tag_type)
+		entry_group["type"] = tag_type
+		tag_type = ""
+
+		for entry_text in re.split(common_entry_ptrn, raw_text):
 			if not entry_text:
 				continue
 
 			entry = OrderedDict()
-			
-			if not _match(entry_text):
+
+			if not _match(entry_text, common_entry_ptrns):
 				entry_text, b64 = u_test(entry_text)
 				entry["text"] = entry_text
 				if b64:
 					entry["b64_encoded"] = b64
 			else:
 				entry["data"] = b64encode(entry_text)
-				
+
 			entry_group["entries"].append(entry)
 
-			
+
 		res.append(entry_group)
-		
+
 	return header, res
 	
 def write_xml(out_file, header, l_groups):
@@ -107,12 +116,11 @@ def write_xml(out_file, header, l_groups):
 
 	root = ET.Element("root")
 	doc = ET.SubElement(root, "doc")
-	t_tbl_tag = header["type"]
-	ET.SubElement(doc, HEADER_TAG, type = t_tbl_tag).text = header["data"]
+	ET.SubElement(doc, HEADER_TAG).text = header["data"]
 	
 	idx = 0
 	for l_group in l_groups:
-		el_group = ET.SubElement(doc, t_tbl_tag, data = l_group["data"], idx = "%d" % idx)	
+		el_group = ET.SubElement(doc, GROUP_TAG, type = l_group["type"], data = l_group["data"], idx = "%d" % idx)	
 		for l_entry in l_group["entries"]:
 			if "data" in l_entry:
 				ET.SubElement(el_group, ENTRY_TAG, data = l_entry["data"]).text = ""
@@ -128,22 +136,20 @@ def write_xml(out_file, header, l_groups):
 	indent(root)
 	open(out_file, "wb").write(ET.tostring(root, encoding='utf8', method='xml'))
 	
-def read_xml(in_file, t_tbl_tag = ""):
+def read_xml(in_file):
 	"""Read XML file containing parsed *.tbl file data.
 	Returns *.tbl header and list of OrderedDict's representing each text or binary data entry."""
 
 	res = []
 	root = ET.parse(in_file).getroot()
 	doc = root.find("doc")
-	el_header = doc.find(HEADER_TAG)
-	if not t_tbl_tag:
-		t_tbl_tag = _get_tbl_tag(in_file, is_tbl_file = False)
-		
-	el_groups = doc.findall(t_tbl_tag)
+	el_header = doc.find(HEADER_TAG)	
+	el_groups = doc.findall(GROUP_TAG)
 	for el_group in el_groups:
 		entry_group = OrderedDict()
 		entry_group["entries"] = []
 		entry_group["data"] = el_group.get("data")
+		entry_group["type"] = el_group.get("type")
 		el_entries = el_group.findall(ENTRY_TAG)
 		for el_entry in el_entries:
 			entry = OrderedDict()
@@ -161,7 +167,6 @@ def read_xml(in_file, t_tbl_tag = ""):
 
 	header = OrderedDict()
 	header["data"] = el_header.text
-	header["type"] = t_tbl_tag
 	return header, res
 	
 def write_tbl(out_file, header, l_groups):
@@ -172,10 +177,9 @@ def write_tbl(out_file, header, l_groups):
 	
 	res = []
 	res.append(b64decode(header["data"]))
-	t_tbl_tag = header["type"]
 	
 	for l_group in l_groups:
-		res.append(t_tbl_tag)
+		res.append(l_group["type"])
 		res.append(b64decode(l_group["data"]))
 		for l_entry in l_group["entries"]:
 			if "data" in l_entry:
