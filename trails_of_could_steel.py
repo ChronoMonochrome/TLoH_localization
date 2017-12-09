7e4d0e38ad268d785b68882460b3583f5a6b5e31
Shilin Victor <chrono.monochrome@gmail.com>
2017-12-09 15:43:31 +0300

    Implement an usage function

--- trails_of_could_steel.py
+++ trails_of_could_steel.py
@@ -11,7 +11,7 @@ _main_tbl_ptrns = ["\x00+", "\x1d", ".?\x01", "%[ds]",    \
                    "#[0-9]*[CI]", "#[0-9a-f]*/*", "[\x01-\x09\x0b-\x1c]+"]
 
 _item_tbl_ptrns = [".*\xff\xff"]
-				   
+
 def _main_tbl_split(s):
 	if s.find("NONE") == -1:
 		text_start = [m.end() for m in re.finditer('\x00+', s)][-2]
@@ -20,7 +20,7 @@ def _main_tbl_split(s):
 	data = s[:text_start]
 	text = s[text_start:]
 	return data, text
-		
+
 def _item_tbl_split(s):
 	#if len(s) >= 55:
 	#	return s[:55], s[55:]
@@ -156,25 +156,25 @@ _tbl_to_params = OrderedDict([('t_main',                           \
                                translate._split]                   \
                               ),                                   \
                              ])
-                            
+
 def _read_xml(file):
 	return translate.read_xml(file)
-	
+
 def _read_dat(file):
 	return translate.read_dat(file)
 
 def _read_tbl(file):
 	name = os.path.split(file)[-1]
-	
+
 	found = False
 	for key, params in _tbl_to_params.items():
 		if file.find(key) != -1:
 			found = True
 			break
-			
+
 	assert(found)
 	return translate.read_tbl(file, *params)
-	
+
 def _read_file(file):
 	ext = os.path.splitext(file)[-1]
 	assert(ext in [".tbl", ".xml", ".dat"])
@@ -191,33 +191,33 @@ def xml_to_tbl(in_file, out_file = ""):
 	if not out_file:
 		out_file = os.path.splitext(in_file)[0] + ".tbl"
 	translate.write_tbl(out_file, header, l_groups)
-	
+
 def tbl_to_xml(in_file, out_file = ""):
 	header, l_groups = _read_file(in_file)
-	
+
 	if not out_file:
 		out_file = os.path.splitext(in_file)[0] + ".xml"
 	translate.write_xml(out_file, header, l_groups)
-	
+
 def dat_to_xml(in_file, out_file = ""):
 	header, l_groups = _read_file(in_file)
 	if not out_file:
 		out_file = os.path.splitext(in_file)[0] + ".xml"
 	translate.write_xml(out_file, header, l_groups)
-	
+
 def xml_to_dat(in_file, out_file = ""):
 	header, l_groups = translate.read_xml(in_file)
 
 	if not out_file:
 		out_file = os.path.splitext(in_file)[0] + ".dat"
 	translate.write_tbl(out_file, header, l_groups)
-	
+
 def _get_out_filename(in_filename, in_ext):
 	if (in_ext == ".tbl"):
 		return in_filename + ".xml"
 	elif (in_ext == ".xml"):
 		return in_filename + ".tbl"
-	
+
 def convert(in_file, out_file):
 	header, l_groups = _read_file(in_file)
 
@@ -227,25 +227,25 @@ def convert(in_file, out_file):
 		translate.write_xml(out_file, header, l_groups)
 	elif (_in_ext == ".xml"):
 		translate.write_tbl(out_file, header, l_groups)
-		
+
 def wrap_text(in_file, out_file):
 	header, l_groups = _read_file(in_file)
-		
+
 	l_groups = translate.wrap_text(l_groups)
-	
+
 	_out_file, _out_ext = os.path.splitext(out_file)
 
 	if (_out_ext == ".xml"):
 		translate.write_xml(out_file, header, l_groups)
 	elif (_out_ext == ".tbl"):
 		translate.write_tbl(out_file, header, l_groups)
-	
+
 def merge_tbl(orig_file, data_file, out_file):
 	out_format = os.path.splitext(out_file)[-1]
 	assert(out_format in [".tbl", ".xml"])
 	header1, res1 = _read_file(orig_file)
 	header2, res2 = _read_file(data_file)
-	
+
 	n = min(len(res1), len(res2))
 	for i in xrange(n):
 		res1[i]["data"] = res2[i]["data"]
@@ -254,44 +254,83 @@ def merge_tbl(orig_file, data_file, out_file):
 		translate.write_xml(out_file, header1, res1)
 	elif out_format == ".tbl":
 		translate.write_tbl(out_file, header1, res1)
-		
+
 def dump_text(in_file, out_file):
 	header, l_groups = _read_file(in_file)
 	return translate.dump_text(out_file, l_groups)
-	
+
 def dump_data(in_file, out_file):
 	header, l_groups = _read_file(in_file)
 	return translate.dump_data(out_file, l_groups)
-	
+
 def encode(in_file, out_file, out_enc):
 	header, l_groups = _read_file(in_file)
-
-	#l_groups = translate.change_encoding(l_groups, out_enc)
 	translate.write_tbl(out_file, header, l_groups, out_enc)
-	
-def usage():
-	print "fail"
+
+def usage(commands_tbl, error = ""):
+	buf =  ["The Legend of Heroes: Trails of Cold Steel localization scripts\n"]
+	commands = commands_tbl.items()
+	max_com_len = max([len(i[0]) for i in commands])
+	max_args_len = max([sum(len(arg) for arg in i[1][-2]) + len(i[1]) for i in commands])
+	com_list = []
+        for k, v in commands:
+		com = ("  %s %s" % (k.ljust(max_com_len), " ".join(v[-2]))) \
+                                     .ljust(max_com_len + max_args_len)
+		com_list.append("%s - %s" % (com, v[-1]))
+
+        if error: buf.append(error + "\n")
+	buf.append("Usage: trails_of_could_steel.py command args\n")
+	buf.append("Commands:")
+	buf += com_list
+	print("\n".join(buf))
 	exit()
-	
-actions_tbl = {
-	"xml_to_tbl": (xml_to_tbl, 1, 2),
-	"tbl_to_xml": (tbl_to_xml, 1, 2),
-	"dat_to_xml": (dat_to_xml, 1, 2),
-	"xml_to_dat": (xml_to_dat, 1, 2),
-	"merge":      (merge_tbl,  3, 3),
-	"convert":    (convert,    2, 2),
-	"dump_text":  (dump_text,  2, 2),
-	"dump_data":  (dump_data,  2, 2),
-	"wrap":       (wrap_text,  2, 2),
-	"encode":     (encode,     3, 3)
-}
+
+def main():
+	commands_tbl = {
+	        # command       function   min_arg max_arg            args            desc
+		"xml_to_tbl" : [xml_to_tbl,   1,      2,    ["src.xml", "[dest.tbl]"], ""],
+		"tbl_to_xml" : [tbl_to_xml,   1,      2,    ["src.tbl", "[dest.xml]"], ""],
+		"dat_to_xml" : [dat_to_xml,   1,      2,    ["src.dat", "[dest.xml]"], ""],
+		"xml_to_dat" : [xml_to_dat,   1,      2,    ["src.xml", "[dest.dat]"], ""],
+		"merge"      : [merge_tbl,    3,      3,    ["src1.{tbl,xml}",           \
+	                                                     "src2.{tbl,xml}",           \
+	                                                     "dest.{tbl,xml}"       ], ""],
+		"convert"    : [convert,      2,      2,    ["src.{tbl,dat,xml}",        \
+	                                                     "dest.{tbl,dat,xml}"   ], ""],
+		"dump_text"  : [dump_text,    2,      2,    ["src.tbl",   "dest.txt"], ""],
+		"dump_data"  : [dump_data,    2,      2,    ["src.tbl",   "dest.txt"], ""],
+		"wrap"       : [wrap_text,    2,      2,    ["src.{tbl,dat,xml}",        \
+	                                                     "dest.{tbl,dat,xml}"   ], ""],
+		"encode"     : [encode,       3,      3,    ["src1.{tbl,xml}",           \
+	                                                     "dest.{tbl,xml}",           \
+	                                                       "encoding"           ], ""]
+	}
+
+	commands_tbl["xml_to_tbl"][-1] = "convert *.xml file to *.tbl."
+	commands_tbl["tbl_to_xml"][-1] = "convert *.tbl file to *.xml."
+	commands_tbl["dat_to_xml"][-1] = "convert *.dat file to *.xml."
+	commands_tbl["xml_to_dat"][-1] = "convert *.xml file to *.dat."
+	commands_tbl["merge"][-1]      = "use a text entries of src1 and a data entries of src2 and write result to destination file."
+	commands_tbl["convert"][-1]    = "convert *.{tbl,dat,xml} file to dest.{tbl,dat,xml}."
+	commands_tbl["dump_text"][-1]  = "extract text entries from src.tbl and write to dest.txt."
+	commands_tbl["dump_data"][-1]  = "extract data entries from src.tbl and write to dest.txt."
+	commands_tbl["wrap"][-1]       = "wrap text entries in a source file so that they fit on the display."
+	commands_tbl["encode"][-1]     = "change encoding of a source file."
+ 	if len(sys.argv) < 2:
+		usage(commands_tbl)
+
+        command = sys.argv[1]
+
+	if command not in commands_tbl.keys():
+		usage(commands_tbl, error = "%s: command not found" % command)
+	else:
+		command_cb, min_args, max_args, args, desc = commands_tbl[command]
+		if not (min_args <= len(sys.argv) - 2 <= max_args):
+			usage(commands_tbl, error = "%s: from %d to %d arguments are required, but %d provided" \
+                                                            % (command, min_args, max_args, len(sys.argv) - 2))
+		else:
+			command_cb(*sys.argv[2:])
+
 
 if __name__ == "__main__":
-	if sys.argv[1] not in actions_tbl.keys():
-		usage()
-	else:
-		action_cb, min_args, max_args = actions_tbl[sys.argv[1]]
-		if not (min_args <= len(sys.argv) - 2 <= max_args):
-			usage()
-		else:
-			action_cb(*sys.argv[2:])
+	main()
